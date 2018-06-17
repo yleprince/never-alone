e12225833c8bbd12258f4b9c6b686b2002c0644d
christopherbelinguier <christopher.belinguier@gmail.com>
2018-06-17 11:37:04 +0200

    clean GraphSuccessSecondaryFeature

--- js/modules/graphs/GraphSuccessSecondaryFeature.js
+++ js/modules/graphs/GraphSuccessSecondaryFeature.js
@@ -22,7 +22,8 @@ class GraphSuccessSecondaryFeature extends Graph{
         super(id, allData);
 
         let opts = fillWithDefault(options, defaultOptions);
-        this.color = opts.color;
+        // this.color = opts.color;
+        this.color = ["#a6cee3", "#1f78b4"];
         this.size = opts.size;
 
         // Chris
@@ -34,6 +35,7 @@ class GraphSuccessSecondaryFeature extends Graph{
         this.currentContinuousVar = continuousVar[0];
         this.currentCategoricalVar = categoricalVar[1];
         this.filterVar = "gender";
+        this.filterValue = 0;
 
         this.preprocess();
         this.createGraph();
@@ -89,7 +91,7 @@ class GraphSuccessSecondaryFeature extends Graph{
         // Filter sur une autre variable !!!
         this.dataFilterContinuous = this.allData.map(d => {return {tmp_var : d[this.currentContinuousVar], tmp_var_filter : d[this.filterVar]}})
             .filter(d => d.tmp_var > 0)
-            .filter(d => d.tmp_var_filter > 0);
+            .filter(d => d.tmp_var_filter === this.filterValue);
 
 
         // Group data per age and get the counts for each age
@@ -121,8 +123,7 @@ class GraphSuccessSecondaryFeature extends Graph{
         // Get filter data
         // Filter sur une autre variable !!!
         this.dataFilterCategorical = this.allData.map(d => {return {tmp_var : d[this.currentCategoricalVar], tmp_var_filter : d[this.filterVar]}})
-            .filter(d => d.tmp_var_filter > 0);
-
+            .filter(d => d.tmp_var_filter === this.filterValue);
 
         // Group data per age and get the counts for each age
         this.dataFilterCategorical = d3.nest()
@@ -133,6 +134,29 @@ class GraphSuccessSecondaryFeature extends Graph{
 
         console.log("dataFilter Categorical Graph: ");
         console.log(this.dataFilterCategorical);
+
+        // Concatenate array
+        // set array length
+        let nbLine = this.dataFullCategorical.length;
+        let nbColumn = 2;
+        // create array
+        this.dataCategorical = new Array(nbLine);
+        for (var i = 0; i < this.dataCategorical.length; i++) {
+            this.dataCategorical[i] = new Array(nbColumn);
+            this.dataCategorical[i]["key"] = this.dataFullCategorical[i].key; // key full
+            this.dataCategorical[i]["Full"] = this.dataFullCategorical[i].value; // value full
+            this.dataCategorical[i]["Selected"] = 0; // init value filter
+            // value filter
+            for (let elementFilter in this.dataFilterCategorical) {
+                if (this.dataFullCategorical[i].key === this.dataFilterCategorical[elementFilter].key) {
+                    this.dataCategorical[i]["Selected"] = this.dataFilterCategorical[elementFilter].value;
+                    break;
+                }
+            }
+        }
+
+        console.log("dataCategorical Categorical Graph: ");
+        console.log(this.dataCategorical);
     }
 
     /**
@@ -149,7 +173,7 @@ class GraphSuccessSecondaryFeature extends Graph{
 
     createContinuousGraph(){
         // margin continuous var
-        let margin = {top: 50, right: 50, bottom: this.height*(2/3), left: this.width*(3/4)},
+        let margin = {top: 100, right: 100, bottom: this.height*(2/3), left: this.width*(3/4)},
             innerWidth = this.width - margin.left - margin.right,
             innerHeight = this.height - margin.top - margin.bottom;
 
@@ -162,6 +186,8 @@ class GraphSuccessSecondaryFeature extends Graph{
             .range([innerHeight, 0])
             // .domain([0, d3.max(this.dataFullContinuous, function(d) { return d.value; })]);
             .domain(d3.extent(this.dataFullContinuous, d => d.value));
+
+        let z = d3.scaleOrdinal().range(this.color.reverse());
 
         let g = this.svg.append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -177,9 +203,9 @@ class GraphSuccessSecondaryFeature extends Graph{
             .attr("class", "line")
             .attr("d", lineFull)
             .style("fill", "None")
-            .style("stroke", "red");
+            .style("stroke", z);
 
-        // Filter data
+        // Selected data
         let lineFilter = d3.line()
             .curve(d3.curveBasis)
             .x(function(d) { return x(d.key); })
@@ -190,9 +216,8 @@ class GraphSuccessSecondaryFeature extends Graph{
             .attr("class", "line")
             .attr("d", lineFilter)
             .style("fill", "None")
-            .style("stroke", "darkgray");
+            .style("stroke", z);
 
-        // Legend
         // x axis
         g.append("g")
             .attr("class", "x axis")
@@ -205,93 +230,68 @@ class GraphSuccessSecondaryFeature extends Graph{
             // .attr("transform", "translate(0, " + innerWidth + ")")
             .call(d3.axisLeft(y));
 
-        // Focus
+        // Keys
+        let keys = ["key", "Full", "Selected"];
 
+        // Legend
+        let legend = g.append("g")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 10)
+            .attr("text-anchor", "end")
+            .selectAll("g")
+            .data(keys.slice(1))
+            .enter().append("g")
+            .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+        legend.append("rect")
+            .attr("x", innerWidth - 19)
+            .attr("width", 19)
+            .attr("height", 19)
+            .attr("fill", z);
+
+        legend.append("text")
+            .attr("x", innerWidth - 24)
+            .attr("y", 9.5)
+            .attr("dy", "0.32em")
+            .text(function(d) { return d; });
     }
 
 
     createCategoricalVar(){
         // margin continuous var
-        let margin = {top: this.height*(2/3), right: 50, bottom: 50, left: this.width*(3/4)},
+        let margin = {top: this.height*(2/3), right: 100, bottom: 100, left: this.width*(3/4)},
             innerWidth = this.width - margin.left - margin.right,
             innerHeight = this.height - margin.top - margin.bottom;
-
-        const x = d3.scaleBand()
-                    .rangeRound([0, innerWidth])
-                    .padding(0.1)
-                    .domain(this.dataFullCategorical.map(function(d) { return d.key; }));
-
-        const y = d3.scaleLinear()
-                    .rangeRound([innerHeight, 0])
-                    .domain([0, d3.max(this.dataFullCategorical, function(d) { return d.value; })]);
 
         let g = this.svg.append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        // Full data
-        g.selectAll(".bar")
-            .data(this.dataFullCategorical)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", function(d) { return x(d.key); })
-            .attr("y", function(d) { return y(d.value); })
-            .attr("width", x.bandwidth())
-            .attr("height", function(d) { return innerHeight - y(d.value); })
-            .style("fill", "red");
-
-        // Filter data
-        g.selectAll(".bar")
-            .data(this.dataFilterCategorical)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", function(d) { return x(d.key); })
-            .attr("y", function(d) { return y(d.value); })
-            .attr("width", x.bandwidth())
-            .attr("height", function(d) { return innerHeight - y(d.value); })
-            .style("fill", "darkgray");
-
-        // Legend
-        // x axis
-        g.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + innerHeight + ")")
-            .call(d3.axisBottom(x));
-
-        // y axis
-        g.append("g")
-            .attr("class", "y axis")
-            // .attr("transform", "translate(0, " + innerWidth + ")")
-            .call(d3.axisLeft(y));
-
-        // Manon
-        /*let x0 = d3.scaleBand()
-            .rangeRound([0, innerWidth])
-            .paddingInner(0.1);
-
-        let x1 = d3.scaleBand()
-            .padding(0.05);
-
-        let y = d3.scaleLinear()
-            .rangeRound([innerHeight, 0]);
-
-        let z = d3.scaleOrdinal()
-            .range(["#a6cee3", "#1f78b4", "#b2df8a"])
-
-        let keys = d3.keys(this.dataFilterCategorical.key);
+        let keys = d3.keys(this.dataCategorical[0]);
         console.log("key: " + keys);
 
+        let x0 = d3.scaleBand()
+            .rangeRound([0, innerWidth])
+            .paddingInner(0.1)
+            .domain(this.dataCategorical.map(function(d) {return d["key"]; }));
 
-        x0.domain(this.data[0].map(function(d) {return d.Attribute; }));
-        x1.domain(keys.slice(1)).rangeRound([0, x0.bandwidth()]);
+        let x1 = d3.scaleBand()
+            .padding(0.05)
+            .domain(keys.slice(1)).rangeRound([0, x0.bandwidth()]);
+
+        let y = d3.scaleLinear()
+            .rangeRound([innerHeight, 0])
+            .domain([0, d3.max(this.dataCategorical, function(d) { return d["Full"]; })]);
+
+        let z = d3.scaleOrdinal().range(this.color.reverse());
 
         // Plot the bars
         g.append("g")
             .selectAll("g")
-            .data(this.dataFilterCategorical)
+            .data(this.dataCategorical)
             .enter().append("g")
-            //.attr("transform", function(d) { return "translate(" + x0(d.Attribute) + ",0)"; })
+            .attr("transform", function(d) { return "translate(" + x0(d["key"]) + ",0)"; })
             .selectAll("rect")
-            .data(function(d) { return keys.slice(1).map(function(key) { return {key: key, value: d[key]}; }); })
+            .data(function(d) { return keys.map(function(key) { return {key: key, value: d[key]}; }); })
             .enter().append("rect")
             .attr("x", function(d) { return x1(d.key); })
             .attr("y", function(d) { return y(d.value); })
@@ -309,11 +309,10 @@ class GraphSuccessSecondaryFeature extends Graph{
 
                 d3.select("#id" + i).remove();});
 
-
         // Add horizontal axis with name of the attributes
         g.append("g")
             .attr("class", "axis")
-            .attr("transform", "translate(0," + cfg.height + ")")
+            .attr("transform", "translate(0," + innerHeight + ")")
             .call(d3.axisBottom(x0));
 
 
@@ -328,8 +327,9 @@ class GraphSuccessSecondaryFeature extends Graph{
             .attr("fill", "#000")
             .attr("font-weight", "bold")
             .attr("text-anchor", "start")
-            .text("Score")
+            .text("Population");
 
+        // Legend
         let legend = g.append("g")
             .attr("font-family", "sans-serif")
             .attr("font-size", 10)
@@ -340,16 +340,16 @@ class GraphSuccessSecondaryFeature extends Graph{
             .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
         legend.append("rect")
-            .attr("x", cfg.width - 19)
+            .attr("x", innerWidth - 19)
             .attr("width", 19)
             .attr("height", 19)
             .attr("fill", z);
 
         legend.append("text")
-            .attr("x", cfg.width - 24)
+            .attr("x", innerWidth - 24)
             .attr("y", 9.5)
             .attr("dy", "0.32em")
-            .text(function(d) { return d; });*/
+            .text(function(d) { return d; });
     }
 
 
